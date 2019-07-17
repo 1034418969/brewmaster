297d209e4538241f987845b75d81eb7ba58f2df8
lixinyue <2294998274@qq.com>
2019-07-17 19:17:29 +0800

    goods用redux

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../assets/css/Detail.css';
 import {
+  connect
+} from "react-redux"
+import {
   BrowserRouter as Router,
   Route,
   NavLink
@@ -10,38 +13,52 @@ import iconfont from "../assets/font/iconfont.css"
 import Connect from "../views/Detail/Connect"
 import Shop from "../views/Detail/Shop"
 import Car from "../views/Detail/Car"
-import JionCar  from "../views/Detail/JionCar"
+import JionCar from "../views/Detail/JionCar"
 import Buy from "../views/Detail/Buy"
 import Default from '../views/Detail/Default';
 
-class Detail extends React.Component{
-  render(){
-  return (
-    <div className="App">
-     <div className="last">
-       
-       <Router>
-         <Route path={"/detail"} component={Default} exact></Route>
-         <Route path={"/connect"} component={Connect}></Route>
-         <Route path={"/shop"} component={Shop}></Route>
-         <Route path={"/car"} component={Car}></Route>
-         <Route path={"/jioncar"} component={JionCar}></Route>
-         <Route path={"/buy"} component={Buy}></Route>
-         
-         <NavLink exact to={"/detail"}></NavLink>
-         <div className="qwe">
-         <NavLink to={"/connect"} className="iconfont icon-kefu liney"><span className="size">在线收藏</span></NavLink>
-         <NavLink to={"/shop"}  className="iconfont icon-shoucang2 liney" ><span className="size">收藏</span></NavLink>
-         <NavLink to={"/car"} className="iconfont icon-gouwuche line_1"><span className="size">购物车</span></NavLink>
-         <NavLink to={"/jioncar"} className="jion">加入购物车</NavLink>
-         <NavLink to={"/buy"} className="gobuy" >立即购买</NavLink>
-         </div>
-       </Router>
-     </div>
-    </div>
-  )
+class Detail extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div className="App">
+        {
+          //   <div className="last">
+
+          //   <Router>
+          //     <Route path={"/detail"} component={Default} exact></Route>
+          //     <Route path={"/connect"} component={Connect}></Route>
+          //     <Route path={"/shop"} component={Shop}></Route>
+          //     <Route path={"/car"} component={Car}></Route>
+          //     <Route path={"/jioncar"} component={JionCar}></Route>
+          //     <Route path={"/buy"} component={Buy}></Route>
+
+          //     <NavLink exact to={"/detail"}></NavLink>
+          //     <div className="qwe">
+          //     <NavLink to={"/connect"} className="iconfont icon-kefu liney"><span className="size">在线收藏</span></NavLink>
+          //     <NavLink to={"/shop"}  className="iconfont icon-shoucang2 liney" ><span className="size">收藏</span></NavLink>
+          //     <NavLink to={"/car"} className="iconfont icon-gouwuche line_1"><span className="size">购物车</span></NavLink>
+          //     <NavLink to={"/jioncar"} className="jion">加入购物车</NavLink>
+          //     <NavLink to={"/buy"} className="gobuy" >立即购买</NavLink>
+          //     </div>
+          //   </Router>
+          // </div>
+        }
+      </div>
+    )
   }
- 
+  componentDidMount(){
+    console.log(this.props.goods)
+  }
+}
+function mapStateToProps(state) {
+  console.log(state,11111111)
+  return {
+      list: state.clear.list,
+      pageNum: state.clear.pageNum,
+      isLoading:state.clear.isLoading,
+      saleAll:state.clear.saleAll
+  }
 }
 
-export default Detail;
+export default connect(mapStateToProps,null)(Detail);
