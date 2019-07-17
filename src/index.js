eb92a86a41f90d375a4b1c10ae080d538baed02c
JiaHao Li <1034418969@qq.com>
2019-07-17 13:57:31 +0800

    Car

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import store from "./store"
 import App from './App';
-import {
-    Provider
-}from "react-redux";
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
 import {
     Provider
 } from "react-redux";
-import store from "./store"
-=======
-
->>>>>>> 8b6332137fe18639796465eb9e42119591a66ed3
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
