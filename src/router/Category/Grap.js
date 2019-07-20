e3c79d4b5221c1be242b92b04e0ed18e3a767390
houyingjun <445913891@qq.com>
2019-07-20 18:09:05 +0800

    upCategory

--- src/router/Category/Grap.js
+++ src/router/Category/Grap.js
@@ -1,19 +1,35 @@
 import React from "react";
 import axios from "axios"
+import ClearAction from "../../store/actionCreator/clear"
+import {
+    NavLink
+}from "react-router-dom"
+import {
+    bindActionCreators
+} from "redux"
+import {
+    connect
+} from "react-redux"
+
+
 class Grap extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            goodsList:[],
-            isShow:true
+            goodsList: [],
+            isShow: true
         }
     }
+
     render() {
-        return(
+        return (
             <div className="grap">
                 <div className="headSearch">
-                    <span className="iconfont icon-fanhui" onClick={()=>{this.props.history.push("/category")}}></span> <b className="iconfont icon-search" id="search_text">全场商品低至9.9元起</b><span className="iconfont icon-caidan" onClick={()=>{
+                    <span className="iconfont icon-fanhui" onClick={() => {
+                        this.props.history.push("/category")
+                    }}></span> <b className="iconfont icon-search" id="search_text">全场商品低至9.9元起</b><span
+                    className="iconfont icon-caidan" onClick={() => {
                     console.log(11111)
                     // this.setState({
                     //     isShow:!this.state.isShow
@@ -25,7 +41,8 @@ class Grap extends React.Component {
                 <div className="tabBar">
                     <span>综合</span>
                     <span>销量</span>
-                    <span className="iconfont">价格<b className="iconfont icon-xiaosanjiao top"></b> <b className="iconfont icon-xiaosanjiao1 bottom"></b></span>
+                    <span className="iconfont">价格<b className="iconfont icon-xiaosanjiao top"></b> <b
+                        className="iconfont icon-xiaosanjiao1 bottom"></b></span>
                     <span className="line">|</span>
                     <span>筛选</span>
                 </div>
@@ -35,28 +52,31 @@ class Grap extends React.Component {
                 </div>
                 <hr className="hr"/>
                 {/*{*/}
-                    {/*this.state.isShow?<Detail id={this.props.match.params.id}></Detail>:<Two></Two>*/}
+                {/*this.state.isShow?<Detail id={this.props.match.params.id}></Detail>:<Two></Two>*/}
                 {/*}*/}
 
                 <div>
                     {
-                        this.state.goodsList.map((v,i)=>{
-                            return(
-                                <div id="houImg" key={v.goods_id}>
-                                    <div className="houImg"><img src={v.goods_thumb} alt=""/></div>
-                                    <div id="houWord">
-                                        <section>
-                                            <p>{v.goods_name}</p>
-                                        </section>
-                                        <section>
-                                            <b id="price">&yen;{v.cur_price}.00</b>
-                                            <p id="self"><b>酒</b><i>自营</i><em>{v.goods_id}人评论</em></p>
-                                        </section>
+                        this.state.goodsList.map((v, i) => {
+                            return (
+                                <NavLink key={i} onClick={this.props.toDetail.bind(this,v)} to="/detail">
+                                    <div id="houImg" key={v.goods_id}>
+                                        <div className="houImg"><img src={v.goods_thumb} alt=""/></div>
+                                        <div id="houWord">
+                                            <section>
+                                                <p>{v.goods_name}</p>
+                                            </section>
+                                            <section>
+                                                <b id="price">&yen;{v.cur_price}.00</b>
+                                                <p id="self"><b>酒</b><i>自营</i><em>{v.goods_id}人评论</em></p>
+                                            </section>
+                                        </div>
                                     </div>
-                                </div>
+                                </NavLink>
                             )
                         })
                     }
+
                 </div>
             </div>
         )
@@ -66,6 +86,7 @@ class Grap extends React.Component {
         console.log(this.props.match.params)//{id:1}
         this.getGoodsList()
     }
+
     //
     getGoodsList() {
         axios.get("https://m.jiuxian.com/m_v1/dynamic/mob01ajax/151746?pageNum=2")
@@ -79,4 +100,19 @@ class Grap extends React.Component {
 
 }
 
-export default Grap;
+function mapStateToProps(state) {
+    // console.log(state.goods)
+    return {
+        goods:state.goods
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(ClearAction,dispatch)
+    // {//另一种写法
+    //     getClearList(pageNum = 1) {
+    //         dispatch(ClearAction.getClearList(pageNum))
+    //     }
+    // }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Grap);
